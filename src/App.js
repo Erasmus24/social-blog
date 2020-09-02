3789745c46724993912bbd5fcece136cd5a90d86
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-09-02 04:21:12 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>This is a Social blog!!</h1>
+        <h1 className='App-logo'>This is a Social blog!!</h1>
       </header>
     </div>
   );
